94480a99b8f2e348f02cfafa3f105fbbfd345be7
Nico <Nico.Natividad@sdgku.edu>
2026-07-01 21:19:05 -0800

    n display

---
+++ my-react-store/src/App.jsx
@@ -0,0 +1,13 @@
+import './components/Navbar.jsx'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <Navbar />
+      <h1>Welcome to my online store</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ my-react-store/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css"
+
+
+function Navbar() {
+    return (
+       <h1>Welcome to my online store</h1>
+    )
+}
+
+export default Navbar;
