027772c299954f10930be33f923f1f00331209e7
Nico <Nico.Natividad@sdgku.edu>
2026-07-16 12:44:27 -0800

    sdsd

--- my-react-store/src/App.jsx
+++ my-react-store/src/App.jsx
@@ -1,24 +1,33 @@
+import {BrowserRouter, Routes, Route} from 'react-router';
+
 import Navbar from './components/Navbar.jsx'; 
 import './components/Footer.jsx'
 
-
+import Home from './pages/Home.jsx';
 import './App.css';
 import Footer from './components/Footer.jsx';
 import Catalog from './components/Catalog.jsx';
 import Product from './components/Product.jsx';
 import QuantityPicker from './components/QuantityPicker.jsx';
+import About from './pages/Contact.jsx'
+
 
 function App() {
   return(
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <h1 style={{ textAlign: 'center', fontSize: '1.2rem', fontFamily: 'Arial, sans-serif' }}>Shop with Fashion</h1>
-      <div className="catalog-container">
-        <Catalog />
-      </div>
+        <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/about" element = {<About/>}/>
+        <Route path ="/contact"element={<Contact/>} />
+        <Route path = "/catalog" element = {<Catalog/>}/>
+        <Route path = "*" element = {<h1>404 page not found</h1>}/>
+        </Routes>
+       
+      
       
       <Footer/>
-    </div>
+    </BrowserRouter>
   ); 
 } 
 
